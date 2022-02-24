7fd4620041516c555eea877d0880c138a3919665
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-02-24 14:04:12 +0100

    [Commit 'A02 Création d'un stateful composable']

---
+++ Exercice1/app/src/main/java/be/vinci/exercice1/MainActivity.kt
@@ -0,0 +1,70 @@
+package be.vinci.exercice1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.TouchApp
+import androidx.compose.material.icons.filled.TrendingUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import be.vinci.exercice1.ui.theme.Exercice1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { //point d'entree du framework
+            Exercice1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(
+                    ) {
+                        printClick(0)
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun printClick(valeur: Int) {
+    Surface(shape = MaterialTheme.shapes.medium, elevation = 3.dp) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp),
+        ) {
+            Text(
+                text = "$valeur clicks",
+                style = MaterialTheme.typography.h6,
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                imageVector = Icons.Default.TouchApp,
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Exercice1Theme {
+        Column() {
+            printClick(0)
+
+        }
+    }
+}
