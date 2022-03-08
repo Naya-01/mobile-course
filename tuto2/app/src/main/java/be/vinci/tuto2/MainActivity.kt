b199011a7eb42b430afd229b1ac1bccffbae1662
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-03-08 10:21:18 +0100

    [Commit 'T24 Navigation entre écrans']

--- tuto2/app/src/main/java/be/vinci/tuto2/MainActivity.kt
+++ tuto2/app/src/main/java/be/vinci/tuto2/MainActivity.kt
@@ -11,20 +11,36 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import be.vinci.tuto2.ui.theme.Tuto2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            val profiles = getProfiles()
             Tuto2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    ProfileList(profiles = getProfiles(), action = {})
+                    NavHost(navController = navController, startDestination = "/profiles")
+                    {
+                        composable("/profiles") {
+                            ProfileList(profiles) { profile ->
+                                navController.navigate("/profile/${profile.id}")
+                            }
+                        }
+                        composable("/profile/{id}") { navBackStackEntry ->
+                            val id = navBackStackEntry.arguments!!.getString("id")!!
+                            val profile = profiles.first { it.id == id.toInt() }
+                            ProfileScreen(profile) { navController.popBackStack() }
+                        }
+                    }
                 }
             }
         }
