38887beb9a38d323d24f5b2833574161f21418d6
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-03-24 14:10:08 +0100

    [Commit 'T42 Données venant d’une API']

---
+++ tuto4/app/src/main/java/be/vinci/tuto4/data/Word.kt
@@ -0,0 +1,5 @@
+package be.vinci.tuto4.data
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Word(val word: String)
---
+++ tuto4/app/src/main/java/be/vinci/tuto4/data/WordViewModel.kt
@@ -0,0 +1,30 @@
+package be.vinci.tuto4.data
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.*
+import io.ktor.client.features.get
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.launch
+
+class WordViewModel: ViewModel() {
+    private val client = HttpClient(Android) {
+        install(JsonFeature) { serializer = KotlinxSerializer() }
+    }
+    private var words = listOf<Word>()
+    var word by mutableStateOf<Word?>(null)
+    init { refresh() }
+    fun refreshWord() { word = words.randomOrNull() }
+    fun refresh() {
+        viewModelScope.launch {
+            words = client.get("http://172.30.0.1/words")
+        }
+    }
+}
