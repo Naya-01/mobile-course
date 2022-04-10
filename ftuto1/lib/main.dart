ce244e7b9e29c9c2a9f36b51145e1e143efbcfa9
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-04-10 17:55:48 +0200

    [Commit 'FT16 image section']

--- ftuto1/lib/main.dart
+++ ftuto1/lib/main.dart
@@ -43,7 +43,6 @@ Widget titleSection = Container(
   ),
 );
 
-
 Column _buildButtonColumn(Color color, IconData icon, String label) {
   return Column(
     mainAxisSize: MainAxisSize.min,
@@ -69,11 +68,11 @@ Widget textSection = const Padding(
   padding: EdgeInsets.all(32),
   child: Text(
     'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
     softWrap: true,
   ),
 );
@@ -94,14 +93,20 @@ class MyApp extends StatelessWidget {
       ],
     );
 
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
@@ -111,10 +116,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
 
 class RandomsWords extends StatefulWidget {
   const RandomsWords({Key? key}) : super(key: key);
