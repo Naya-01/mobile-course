f7ec7c015336d6f2b621ca79b0df0784e151fb1e
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-04-19 10:01:13 +0200

    [Commit 'FT23 gestion de l’état chez le parent']

---
+++ ftuto2/android/app/src/main/kotlin/be/vinci/ftuto2/MainActivity.kt
@@ -0,0 +1,6 @@
+package be.vinci.ftuto2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ftuto2/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ClickManager(
+          clickCount: _counter,
+          onTapText: _incrementCounter,
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ClickManager extends StatelessWidget {
+  final int clickCount;
+  final onTapText;
+
+  const ClickManager({Key? key, this.clickCount = 0, this.onTapText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: onTapText,
+          child: const Text(
+            'You have pushed the button or this text this many times:',
+          ),
+        ),
+        Text(
+          '$clickCount',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
+}
